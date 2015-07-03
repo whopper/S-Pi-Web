e3182ee732ac101aab1e658fb621f605cab22637
Will Hopper <whopper@puppetlabs.com>
2015-07-02 18:03:23 -0700

    (DEMO) Integrate Jason's Vertx backend with the overview dashboard

--- flot_graphs/flot_graphs.js
+++ flot_graphs/flot_graphs.js
@@ -63,3 +63,68 @@ function main(id) {
   // Add the Flot version string to the footer
   $("#footer").prepend("Flot " + $.plot.version + " &ndash; ");
 };
+
+
+function integrate(id) {
+  // We use an inline data source in the example, usually data would
+  // be fetched from a server
+  console.log(id);
+  var data = [],
+  totalPoints = 300;
+
+  var eb = new vertx.EventBus("http://s-pi-demo.com/api/streambus");
+
+   var updateInterval = 30;
+   $("#updateInterval").val(updateInterval).change(function () {
+     var v = $(this).val();
+     if (v && !isNaN(+v)) {
+       updateInterval = +v;
+       if (updateInterval < 1) {
+         updateInterval = 1;
+       } else if (updateInterval > 2000) {
+         updateInterval = 2000;
+       }
+       $(this).val("" + updateInterval);
+     }
+   });
+
+   eb.onopen = function() {
+     $.when($.ajax("http://s-pi-demo.com/api/stream/numerical/hr/1"),
+     $.ajax("http://s-pi-demo.com/api/stream/numerical/hr/2"),
+     $.ajax("http://s-pi-demo.com/api/stream/numerical/hr/3"),
+     $.ajax("http://s-pi-demo.com/api/stream/numerical/hr/4")).done(
+      function(dat1) {
+        eb.registerHandler(dat1[0], function(msg) {
+          var dmsg = JSON.parse(msg);
+          console.log("getting msg x: " + dmsg.x + " y: " + dmsg.y);
+
+          if (dmsg.y < 0) {
+            dmsg.y = 0;
+          } else if (dmsg.y > 100) {
+            dmsg.y = 100;
+          }
+
+          data.push([dmsg.x, dmsg.y]);
+          if (data.length > 15)
+            data.shift();
+          console.log(data);
+
+              var plot = $.plot(id, [ data ], {
+                series: {
+                  shadowSize: 0 // Drawing is faster without shadows
+                },
+                yaxis: {
+                  min: 0,
+                  max: 100
+                },
+                xaxis: {
+                  show: false
+                },
+                colors: ["#00FF33", "#00FF33"],
+              });
+              $("#footer").prepend("Flot " + $.plot.version + " &ndash; ");
+        })
+      }
+    )
+  }
+}
