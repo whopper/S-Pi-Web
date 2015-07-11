6178b4fae44dfbc98bf60f000437c36d216ecd17
Peter Pokorny <pmpokorny@gmail.com>
2015-07-11 16:56:10 -0700

    (WEB-60) Initial commit before experimental changes.

--- smoothie_graphs/patient_graphs.js
+++ smoothie_graphs/patient_graphs.js
@@ -1,14 +1,17 @@
 // JavaScript function for calling the api to get the graph data
 // points and populating the flot graphs. This fucntion is specifically.
 // for the ecg graphs on the patient graphs.
+var currentGraphs = [];
 function detail_graphs(eb) {
   var currentBuffers = {};
-  var currentGraphs = [];
   var neededGraphs = [];
   neededGraphs.push(['waveform', 'bp', 1]);
   neededGraphs.push(['waveform', 'bp', 2]);
   neededGraphs.push(['waveform', 'bp', 3]);
   neededGraphs.push(['waveform', 'bp', 4]);
+
+  console.log("hi");
+  console.log(neededGraphs);
 
   var startGraph = function (stream, type, id) {
     $.when($.ajax('http://api.s-pi-demo.com/stream/'+stream+'/'+type+'/'+id)).done(
@@ -16,17 +19,18 @@ function detail_graphs(eb) {
         var channelName = data;
         var startTime = Date.now();
         currentBuffers[channelName] = new Array();
-        var series = makeSmoothie('chart' + id);
+        var chart = makeSmoothie('chart' + id);
         currentGraphs.push({"channel": channelName,
           "startTime": startTime,
           "buffer": currentBuffers[channelName],
-          "graph": series});
+          "graph": chart.series,
+          "chart": chart.chart});
         eb.registerHandler(channelName, function(msg) {
           currentBuffers[channelName].push(msg.data);
         });
       }
     )
-  }
+  };
 
   var makeSmoothie = function (id) {
     console.log("id" + id)
@@ -35,8 +39,8 @@ function detail_graphs(eb) {
     var series = new TimeSeries();
     chart.addTimeSeries(series, {lineWidth:0.7,strokeStyle:'green'});
     chart.streamTo(canvas, 1720);
-    return series;
-  }
+    return {"series": series, "chart": chart};
+  };
 
   var drawIt = function () {
     currentGraphs.forEach(function (item, idx, thisArray) {
@@ -45,15 +49,25 @@ function detail_graphs(eb) {
         for (var i = data.length - 1; i >= 0; i--) {
           item["graph"].append(item["startTime"], data[i].SIGNAL);
           item["startTime"] += 8;
-        };
+        }
       }
     });
-  }
+  };
 
   eb.onopen = function () {
     for (var i = neededGraphs.length - 1; i >= 0; i--) {
       startGraph(neededGraphs[i][0], neededGraphs[i][1], neededGraphs[i][2]);
-    };
+    }
     setInterval(drawIt, 400);
+    console.log('about to call resize');
+    handleResize;
   }
 }
+var handleResize = function () {
+  for (var i = 0; i < currentGraphs.length; i++) {
+    console.log('resized' + i);
+    var mycanvas = currentGraphs[i].chart.canvas;
+    mycanvas.width = mycanvas.parentNode.offsetWidth;
+    currentGraphs[i].chart.resize();
+  }
+};
