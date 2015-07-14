66fc0dcd26faedabdccc68dd478ea2195791ad66
Carly Thelander <tcarly@pdx.edu>
2015-07-13 17:17:56 -0700

    (WEB-53)Implement alert stack
    Work in progress, I have made the alert fields dynamic but there
    are some small problems with how they load but only speraticlly.

--- smoothie_graphs/overview_graphs.js
+++ smoothie_graphs/overview_graphs.js
@@ -30,6 +30,8 @@ function ecg_graph(eb) {
     )
   }
 
+
+
   var makeSmoothie = function (id) {
     console.log("id" + id)
     var chart = new SmoothieChart({millisPerPixel:8, strokeStyle:'green'});
@@ -52,27 +54,97 @@ function ecg_graph(eb) {
     });
   }
 
-     function make_alert(dat1, dat2, dat3, dat4) { 
-            console.log("alert")
-            console.log(dat1)
-            console.log(dat2)
-            console.log(dat3)
+    eb.onopen = function () {
+       for (var i = neededGraphs.length - 1; i >= 0; i--) {
+         startGraph(neededGraphs[i][0], neededGraphs[i][1], neededGraphs[i][2]);
+       };
+
+       setInterval(drawIt, 400);
+
+       $.when($.ajax("http://api.s-pi-demo.com/alerts/1"),
+              $.ajax("http://api.s-pi-demo.com/alerts/2"),
+              $.ajax("http://api.s-pi-demo.com/alerts/3"),
+              $.ajax("http://api.s-pi-demo.com/alerts/4")
+       ).done(get_alert);
+    
+    }
+
+    function get_alert(dat1, dat2, dat3, dat4) { 
+        console.log("alert")
 
         eb.registerHandler(dat1[0], function(msg) {
-                $('#alertModal').modal('show');
+            make_alert(msg);
+        });  
+        eb.registerHandler(dat2[0], function(msg) {
+            make_alert(msg);
         });
-     }
+       eb.registerHandler(dat3[0], function(msg) {
+            make_alert(msg);
+        });
+       eb.registerHandler(dat4[0], function(msg) {
+            make_alert(msg);
+        });
 
-  eb.onopen = function () {
-    for (var i = neededGraphs.length - 1; i >= 0; i--) {
-      startGraph(neededGraphs[i][0], neededGraphs[i][1], neededGraphs[i][2]);
-    };
-    setInterval(drawIt, 400);
-          $.when($.ajax("http://api.s-pi-demo.com/alerts/1"),
-                  $.ajax("http://api.s-pi-demo.com/alerts/2"),
-                  $.ajax("http://api.s-pi-demo.com/alerts/3"),
-                  $.ajax("http://api.s-pi-demo.com/alerts/4")
-              ).done(make_alert);
- 
-  }
+    }
+
+    function make_alert(msg) {
+        
+        var self = this;
+        this.alert_msg = msg.alert_msg;
+        this.action_msg = msg.action_msg;
+        this.alert_status = "Active Warning";
+        this.alert_time = msg.ts;
+        this.id = msg.patient_id;
+        this.interval = msg.interval;
+        this.signame = msg.signame;
+        
+        $.getJSON('/patients.json', function(data) {
+            self.name = data['patients'][(self.id-  1)]['name'];
+            self.age =  data['patients'][(self.id - 1)]['age'];
+            self.bed =  data['patients'][(self.id - 1)]['bed'];
+        });
+        
+        console.log(this.name);
+        $("#alert_table").html("\
+               <div class='panel-heading'> Alert:  " + this.name + " -  " + this.alert_msg + " at "+ this.alert_time +" PM </div>\
+                 <div  class='panel-body'>\
+                  <table class='table'>\
+                     <tr>\
+                        <tr>\
+                           <td> <b>Name:</b> </td>\
+                           <td>" + this.name + "</td>\
+                        </tr>\
+                        <tr>\
+                           <td> <b>Age:</b> </td>\
+                           <td>" + this.age + "</td>\
+                        </tr>\
+                        <tr>\
+                           <td> <b>Bed:</b>  </td>\
+                           <td>" + this.bed + "</td>\
+                        </tr>\
+                        <tr>\
+                           <td> <b>Code:</b>  </td>\
+                           <td>" + this.signame + "</td>\
+                        </tr>\
+                        <tr>\
+                           <td> <b>Status:</b>  </td>\
+                           <td>" + this.alert_status + "</td>\
+                        </tr>\
+                        <tr>\
+                           <td> <b>Alert Length:</b>  </td>\
+                           <td>" + this.interval + "</td>\
+                        </tr>\
+                        <tr>\
+                           <td> <b>Alert message:</b>  </td>\
+                           <td>" + this.alert_msg + "</td>\
+                        </tr>\
+                    </table>\
+                   </div>\
+                 ");    
+           
+
+        $('#alertModal').modal('show');
+    }
+
 }
+
